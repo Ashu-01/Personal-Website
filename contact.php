e60c763a77b2592d6b447f0f917cf526071482ea
Ashutosh Verma <79194536+Ashu-01@users.noreply.github.com>
2024-09-27 21:10:58 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,17 +3,21 @@
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
-		$from = 'Ashutosh Contact Form'; 
+		$from = 'From: ' . $email; // Correct the 'From' field
 		$to = 'ashutoshv5390@gmail.com'; 
 		$subject = $_POST['subject']; 
 		
-		$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
-	
-
-	mail($to, $subject, $body, $from) or die("Error!");
-
-	header("location: thank-you.html");
-	
+		$body = "From: $name\nE-Mail: $email\nSubject: $subject\nMessage:\n$message";
+		
+		$headers = "From: $email\r\n"; // Use headers parameter for mail
+		$headers .= "Reply-To: $email\r\n";
+		
+		// Attempt to send the email
+		if (mail($to, $subject, $body, $headers)) {
+			header("Location: thank-you.html"); // Redirect on success
+			exit(); // Ensure script stops after redirection
+		} else {
+			die("Error sending the email!");
+		}
 	}
-	
 ?>
